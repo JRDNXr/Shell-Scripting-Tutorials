a40636de55e1c3b7f08d4c560331f271dfcf3f7a
JRDNXr <jrdn_dsouza@hotmail.com>
2023-01-09 01:00:28 -0500

    Commit 1.0
    - Adding first few tutorials

---
+++ 1.0_Introduction/1.0_HelloWorld.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# We use a single hashtag to comment.
+# To comment out multiple lines, you must continue to use a hashtag.
+
+# The echo command will allow us to output text to the console.
+
+echo Hello World!
---
+++ 1.0_Introduction/1.1_Variables.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# We create variables like so:
+my_var="Hello, World!"
+
+
+
+echo my_var
