9038bdbc18ece12e7cd4cf480ffe4228368d590e
JRDNXr <jrdn_dsouza@hotmail.com>
2023-01-13 01:34:18 -0500

    Commit 1.32
    - Updated tutorials (minor changes)

--- 1.0_Introduction/1.2_Command-Variables.zsh
+++ 1.0_Introduction/1.2_Command-Variables.zsh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-# Command variables are variables that can run Shell commands when called:
-myVar=$(echo "This comman works!")
+# Command variables are variables that can run Shell commands when called. This command will
+# create a file called "test.txt":
+myVar=$(touch test.txt)
 
 # We can call variables by appending a dollar sign ( $ ) before the variable name:
 $myVar
