87b20110a3885744e4eb2549186704e730f876d0
JRDNXr <jrdn_dsouza@hotmail.com>
2023-01-14 00:00:11 -0500

    Commit 1.33
    - Added new tutorial

--- 1.0_Introduction/1.0_HelloWorld.zsh
+++ 1.0_Introduction/1.0_HelloWorld.zsh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
+# The code on Line 1 indicates what type of file this is. It is important to include it in every
+# shell script.
+
 # We use a single hashtag to comment.
-# To comment out multiple lines, you must continue to use a hashtag.
+# To comment out multiple lines, you must append a hashtag on each line.
 
 # The echo command will allow us to output text to the console.
-
 echo Hello World!
---
+++ 1.0_Introduction/1.1_echo.zsh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# The echo command outputs text to the console.
+echo This is a test
+
+
+# The echo command can be very useful when we want to display information, or make changes
+# upon that information.
