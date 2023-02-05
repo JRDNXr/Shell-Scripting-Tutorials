090a9983ba47b873125e946fb9dcb4cc64177427
Jordan D'Souza <jrdn_dsouza@hotmail.com>
2023-02-05 17:12:25 -0500

    Fixed tutorials and uploads

---
+++ 1.0_Introduction/1.5_Arguments.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# We can use arguments in our scripts as well.
+
+# Just before you run this script, add text to the end of the script.
+
+# For example, copy and paste this line:
+# /Users/JRDN/Documents/Coding/Shell/4.0_Learning/1.0_Introduction/1.5_Arguments.zsh Arguments-are-cool!
+
+echo $1
+
+
+# We can utilize arguments to do things in our scripts.
+
+# You can also use as many arguments as you'd like.
+
+
+# /Users/JRDN/Documents/Coding/Shell/4.0_Learning/1.0_Introduction/1.5_Arguments.zsh Arguments-are-cool! The-1st-arg The-2nd-arg
+
+echo "\n"$2
+
+echo "\n"$3
+
+
+# As we increase the number of arguments, we must also increase the nth value. The arguments are passed at the time
---
+++ 2.0_Terminal-Manipulation/2.0_NextTut.zsh
@@ -0,0 +1,4 @@
+
+# PRE_PWD=$(echo /Users/JRDN/Documents/Dr0pbx/Editing/$DATE_COMB | sed -E 's/\-/\./g')
+
+#sed 's/\-/\//'
