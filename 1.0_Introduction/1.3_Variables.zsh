9ba72f5862236c57591e12194a44b3e482f5b128
Jordan D'Souza <jrdn_dsouza@hotmail.com>
2023-03-15 01:28:05 -0400

    Added extra detail to 1.3 tutorial

--- 1.0_Introduction/1.3_Variables.zsh
+++ 1.0_Introduction/1.3_Variables.zsh
@@ -10,6 +10,9 @@ echo $myStr '\n'        ## Output  |  Hello, World!
 myInt=1
 myDecimal=2.64
 
+# NOTE: You cannot have spaces when declaring variables. Ie. myInt = 1 will not work.
+#       Instead, use myInt=1.
+
 echo $myInt             ## Output  |  1
 
 echo $myDecimal         ## Output  |  2.64
